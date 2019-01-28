e6c71bfd56fcdd25821ea622e607f91d8f41dd7d
Tech-a51 <tech-a51@TECH-A51s-MacBook-Air.local>
2019-01-28 16:26:48 +0000

    more data

--- seed.sql
+++ seed.sql
@@ -15,3 +15,16 @@ CREATE TABLE attendence (
   status VARCHAR(25),
   comment VARCHAR (255)
 );
+
+
+INSERT INTO students (firstname, lastname) VALUES ('Studen1t_FN','Student1_LN');
+INSERT INTO students (firstname, lastname) VALUES ('Student2_FN','Student2_LN');
+INSERT INTO students (firstname, lastname) VALUES ('Student3_FN','Student3_LN');
+INSERT INTO students (firstname, lastname) VALUES ('Student4_FN','Student4_LN');
+INSERT INTO students (firstname, lastname) VALUES ('Student5_FN','Student5_LN');
+
+INSERT INTO attendence (dateofattendence, studentid, status, comment) VALUES (now(), 1, 'Authorised Absence', 'Was ill');
+INSERT INTO attendence (dateofattendence, studentid, status, comment) VALUES (now(), 2, 'Authorised Absence', 'Was ill');
+INSERT INTO attendence (dateofattendence, studentid, status, comment) VALUES (now(), 3, 'Authorised Absence', 'Was ill');
+INSERT INTO attendence (dateofattendence, studentid, status, comment) VALUES (now(), 4, 'Authorised Absence', 'Was ill');
+INSERT INTO attendence (dateofattendence, studentid, status, comment) VALUES (now(), 5, 'Authorised Absence', 'Was ill');
