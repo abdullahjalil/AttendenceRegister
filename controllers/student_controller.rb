799179be633d8503ed43ea9db2064756fa2d9bab
Tech-a58 <tech-a58@TECH-A58s-MacBook-Air.local>
2019-01-29 11:28:17 +0000

    added date/status/comments to show page per student

--- controllers/student_controller.rb
+++ controllers/student_controller.rb
@@ -28,7 +28,7 @@ class StudentController < Sinatra::Base
     id = params[:id].to_i
 
     @students = Student.find id
-    @attendence = Attendence.find id
+    # @attendence = Attendence.find id
 
     if(!session[:students])
       session[:students] = []
