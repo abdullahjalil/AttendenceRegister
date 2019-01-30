ffea01f3cf958b6055d5bda959b4c7809ac1babf
Tech-a58 <tech-a58@TECH-A58s-MacBook-Air.local>
2019-01-30 09:36:06 +0000

    fixed css issues on edit page

--- controllers/student_controller.rb
+++ controllers/student_controller.rb
@@ -66,9 +66,9 @@ class StudentController < Sinatra::Base
     attendence = Attendence.new
 
     attendence.date = params[:date]
-    attendence.status = params[:status]
+    attendence.status = params[:status].gsub(/\W/, ' ')
     attendence.studentid = params[:studentid]
-    attendence.comments = params[:comments]
+    attendence.comments = params[:comments].gsub(/\W/, ' ')
 
 
     attendence.save
