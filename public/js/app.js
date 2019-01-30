eef70ac4468c19b83ef5e2681064e37d693b13c0
Tech-a53 <tech-a53@Tech-A53s-MacBook-Air.local>
2019-01-30 10:54:42 +0000

    Created app.js and added function for sorting the table column when you click first name or last name

---
+++ public/js/app.js
@@ -0,0 +1,57 @@
+function sortTable(n) {
+  var table;
+  var rows;
+  var switching;
+  var i;
+  var x;
+  var y;
+  var shouldSwitch;
+  var dir;
+  var switchCount = 0;
+
+  table = document.getElementById("myTable");
+  switching = true;
+
+  dir = "asc";
+
+  while (switching) {
+
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+
+      shouldSwitch = false;
+
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
+
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+
+      switchCount ++;
+    } else {
+
+      if (switchCount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
